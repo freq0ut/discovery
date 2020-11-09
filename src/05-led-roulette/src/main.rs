ff4fdb76bc17e961e31e75298b102cd4f5b93f17
Zack Goyetche <zack.goyetche@protonmail.com>
2020-11-08 22:32:26 -0800

    added config for vscode debug on windows

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,22 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, prelude::*, Delay, Leds};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
+    let period = 50_u16;
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    loop {  
+        for curr in 0..8 {
+            let next = (curr+1) % 8;
+
+            leds[next].on();
+            delay.delay_ms(period);
+
+            leds[curr].off();
+            delay.delay_ms(period);
+        }
+    }
 }
